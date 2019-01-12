56548e720d5b840963e0715aadf2c3a798f4528d
Arda Zaman <ardazamanla@gmail.com>
2019-01-13 00:57:49 +0300

    image

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express')
 var app = express();
+const path = require('path');
+
+app.use(express.static('client'))
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World3')
+})
+
+app.get('/client', function (req, res) {
+  res.sendFile(path.join(__dirname+'/client/index.html'));
 })
  
 app.listen(process.env.PORT || 3000);
