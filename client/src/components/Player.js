08113bed705c8ba8a789e955ceea160a1392f658
Usta <yigitusta@pm.me>
2019-01-24 21:21:56 +0300

    Player class updated

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,11 +1,12 @@
-import Phaser, { Game } from 'phaser';
+import Phaser from 'phaser';
 
 export default class Player extends Phaser.GameObjects.Container {
   constructor(scene, x, y, children) {
     super(scene, x, y, children);
-    const main = scene.add
-      .sprite(0, -12, "atlas", "misa-front");
-    main.name = "main";
+    const main = Object.assign(
+      scene.add.sprite(0, -12, "atlas", "misa-front"),
+      { name: "main" }
+    );
     this.setSize(28, 38);
     this.add(main);
     this.state = {
@@ -42,51 +43,39 @@ export default class Player extends Phaser.GameObjects.Container {
       repeat: -1
     });
   }
-  move(direction) {
-    this.state.prevVelocity = this.body.velocity.clone();
-    this.body.setVelocity(0);
-    switch (direction) {
-      case "left":
-        this._moveLeft();
-      break;
-      case "right":
-        this._moveRight();
-      break;
-      case "up":
-        this._moveUp();
-      break;
-      case "down":
-        this._moveDown();
-      break;
-      default:
-        this._standStill();
-        break;
-    }
-    this.body.velocity.normalize().scale(this.state.speed);
-  }
-  _moveLeft() {
-    this.body.setVelocityX(-this.state.speed);
-    this.getByName("main").anims.play("misa-left-walk", true);
-  }
-  _moveRight() {
-    this.body.setVelocityX(this.state.speed);
-    this.getByName("main").anims.play("misa-right-walk", true);
-  }
-  _moveUp() {
-    this.body.setVelocityY(-this.state.speed);
-    this.getByName("main").anims.play("misa-back-walk", true);
-  }
-  _moveDown() {
-    this.body.setVelocityY(this.state.speed);
-    this.getByName("main").anims.play("misa-front-walk", true);
-  }
-  _standStill() {
+  move({left, right, up, down}) {
     const main = this.getByName("main");
-    const { prevVelocity } = this.state;
-    main.anims.stop();
-    if (prevVelocity.x < 0) main.setTexture("atlas", "misa-left");
-    else if (prevVelocity.x > 0) main.setTexture("atlas", "misa-right");
-    else if (prevVelocity.y < 0) main.setTexture("atlas", "misa-back");
-    else if (prevVelocity.y > 0) main.setTexture("atlas", "misa-front");
+    const prevVelocity = this.body.velocity.clone();
+    this.body.setVelocity(0);
+
+    if (left) {
+      this.body.setVelocityX(-this.state.speed);
+    } else if (right) {
+      this.body.setVelocityX(this.state.speed);
+    }
+
+    if (up) {
+      this.body.setVelocityY(-this.state.speed);
+    } else if (down) {
+      this.body.setVelocityY(this.state.speed);
+    }
+
+    this.body.velocity.normalize().scale(this.state.speed);
+
+    if (left) {
+      main.anims.play("misa-left-walk", true);
+    } else if (right) {
+      main.anims.play("misa-right-walk", true);
+    } else if (up) {
+      main.anims.play("misa-back-walk", true);
+    } else if (down) {
+      main.anims.play("misa-front-walk", true);
+    } else {
+      main.anims.stop();
+      if (prevVelocity.x < 0) main.setTexture("atlas", "misa-left");
+      else if (prevVelocity.x > 0) main.setTexture("atlas", "misa-right");
+      else if (prevVelocity.y < 0) main.setTexture("atlas", "misa-back");
+      else if (prevVelocity.y > 0) main.setTexture("atlas", "misa-front");
+    }
   }
 }
